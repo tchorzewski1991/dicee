8662a792c4980f08c0fb69abadafc8c488513a74
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2020-09-27 17:09:01 +0200

    Setup initial project layout

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dicee'),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
