084d4cc586d1ee8bce80e1a6dce97a329d3e56d2
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2020-09-27 17:32:20 +0200

    Introduce boilerplate for the dice page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,18 +11,54 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
           title: Text(
             'Dicee',
             style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-                fontWeight: FontWeight.w200),
+              color: Colors.white,
+              fontSize: 30.0,
+              fontWeight: FontWeight.w200,
+            ),
           ),
           centerTitle: true,
           backgroundColor: Colors.black,
         ),
-        body: Container(),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'image 1',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+            SizedBox(width: 10.0),
+            Text(
+              'image 2',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
